5218e7e57e196ec822ae2ae92108bf62f3550bb0
Rohan Poudel <66029221+rohanpoudel2@users.noreply.github.com>
2022-03-28 12:43:41 +0545

    Updated the file structure .
    Have updated the file structure
    Separated all the files into different components to make it more easier and readable to our team members 
    Have now started to use php so use xampp for live server to view this website

---
+++ website/js/main.js
@@ -0,0 +1,67 @@
+const navbarList = document.querySelector('.navbar-list');
+
+document.querySelector(".hamburger").onclick = () => {
+  navbarList.classList.add("show");
+}
+
+document.querySelector(".close").onclick = () => {
+  navbarList.classList.remove("show");
+}
+
+//For NavBar Ends
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("hero");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "flex";
+}
+
+const interval = setInterval(function () {
+  plusSlides(1);
+}, 5000);
+
+//For Hero Ends
+
+const body = document.querySelector('#body');
+
+document.querySelector(".light-switch").onclick = () => {
+  body.classList.remove("dark");
+  localStorage.clear();
+  localStorage.setItem("themeStatus", "light");
+}
+
+document.querySelector(".dark-switch").onclick = () => {
+  body.classList.add("dark");
+  localStorage.clear();
+  localStorage.setItem("themeStatus", "dark");
+}
+
+if (localStorage.getItem("themeStatus") == "dark") {
+  body.classList.add("dark");
+}
+
+if (localStorage.getItem("themeStatus") == "light") {
+  body.classList.remove("dark");
+}
+//For Dark Mode Ends
+
