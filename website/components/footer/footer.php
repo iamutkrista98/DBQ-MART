e391d6f619b9ed6c192bc2e00fc16398b5b3763a
iamutkrista98 <66366728+iamutkrista98@users.noreply.github.com>
2022-04-08 22:10:25 +0545

    Update footer.php
    Updated footer information

--- website/components/footer/footer.php
+++ website/components/footer/footer.php
@@ -31,15 +31,15 @@
         <div class="footer-contact-items">
           <div class="footer-contact-item">
             <i class='bx bx-phone-call'></i>
-            <span>123456</span>
+            <span>+441234567</span>
           </div>
           <div class="footer-contact-item footer-contact-middle-item">
             <i class='bx bx-location-plus'></i>
-            <span>Thapathali, Kathmandu</span>
+            <span>Cleckhuddersfax, United Kingdom</span>
           </div>
           <div class="footer-contact-item">
             <i class='bx bx-envelope'></i>
-            <span>dbqmart@rohanpoudel.com</span>
+            <span>dbqmart@gmail.com</span>
           </div>
         </div>
       </div>
