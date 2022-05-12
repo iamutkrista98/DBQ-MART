c171f6b2655d7b060c70c7d6acf69f966f3ebc67
Rohan Poudel <66029221+rohanpoudel2@users.noreply.github.com>
2022-05-12 10:26:27 +0545

    Add files via upload

---
+++ components/customerPage/customerPage.php
@@ -0,0 +1,43 @@
+<section id="customerpage">
+  <div class="container">
+    <div class="customerpage-elements">
+      <div class="customerpage-left">
+        <div class="customerpage-image">
+          <img src="./images/customer/customer.jpeg" alt="">
+        </div>
+        <div class="customerpage-buttons">
+          <button>Edit Details</button>
+          <button>Change Password</button>
+        </div>
+      </div>
+      <div class="customerpage-right">
+        <div class="customerpage-customer-info">
+          <div class="customer-name">
+            <h5>Name:</h5>
+            <h6>Rohan Poudel</h6>
+          </div>
+          <div class="customer-username">
+            <h5>UserName:</h5>
+            <h6>RohanPoudel123</h6>
+          </div>
+          <div class="customer-dob">
+            <h5>Data of Birth:</h5>
+            <h6>12/12/1995</h6>
+          </div>
+          <div class="customer-address">
+            <h5>Address:</h5>
+            <h6>Kathmandu, Tokha</h6>
+          </div>
+          <div class="customer-email">
+            <h5>Email:</h5>
+            <h6>nsrapoudel@gmail.com</h6>
+          </div>
+          <div class="customer-phone-number">
+            <h5>Phone Number:</h5>
+            <h6>9808743723</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
